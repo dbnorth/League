af6ce6357c1a380d60af4aaaae53fadf0492163f
Nic Grounds <nic@nicandnat.com>
2016-07-18 19:16:26 -0500

    Vertically and horizontally center the MainPanel text.

--- src/soccerLeagueUI/MainPanel.java
+++ src/soccerLeagueUI/MainPanel.java
@@ -1,5 +1,7 @@
 package soccerLeagueUI;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -13,11 +15,21 @@ public class MainPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public MainPanel(League league) {
-		setLayout(null);
+		Box vbox = Box.createVerticalBox();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		vbox.add(Box.createVerticalGlue());
+		Box hbox = Box.createHorizontalBox();
+		hbox.add(Box.createHorizontalGlue());
 		
 		JLabel lblNewLabel = new JLabel(league.getName());
-		lblNewLabel.setBounds(112, 112, 230, 16);
-		add(lblNewLabel);
-
+//		lblNewLabel.setBounds(112, 112, 230, 16);
+		hbox.add(lblNewLabel);
+		
+		hbox.add(Box.createHorizontalGlue());
+		vbox.add(hbox);
+		vbox.add(Box.createVerticalGlue());
+		
+		add(vbox);
 	}
 }
